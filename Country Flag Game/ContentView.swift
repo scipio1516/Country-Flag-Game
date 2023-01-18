821d152e1e08336f381bb2e239020514dd42e392
Leo Lai <scipio1516@gmail.com>
2023-01-18 10:03:42 -0600

    Content View gaming

--- Country Flag Game/ContentView.swift
+++ Country Flag Game/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var quizManager = QuizManager()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack(spacing: 40) {
+                VStack(spacing: 20) {
+                    Text ("Country Flag Game")
+                        .font(.title)
+                        .fontWeight(.heavy)
+                        . foregroundColor(.yellow)
+                    Text ("Ready to test your skills?")
+                        .foregroundColor(.yellow)
+                }
+                NavigationLink{
+                    QuestionView()
+                        .environmentObject(quizManager)
+                } label: {
+                    CustomButton(text: "Start")
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .ignoresSafeArea(.all)
+                .background(.cyan)
         }
-        .padding()
     }
 }
 
