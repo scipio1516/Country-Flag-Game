bd446a999aaf85c0699c7e70a63a0a42f44572b9
Leo Lai <scipio1516@gmail.com>
2023-01-18 09:44:48 -0600

    created question view!

---
+++ Country Flag Game/Models/Data.swift
@@ -0,0 +1,8 @@
+//
+//  Data.swift
+//  Country Flag Game
+//
+//  Created by Leo Lai on 1/18/23.
+//
+
+import Foundation
---
+++ Country Flag Game/Views/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  Country Flag Game
+//
+//  Created by Leo Lai on 1/18/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
